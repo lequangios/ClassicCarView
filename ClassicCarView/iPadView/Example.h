fe19d752921675772afb045ff8ddc11a6d259528
Lê Việt Quang <levietquang.ios.dev@gmail.com>
2014-02-14 16:11:06 +0700

    Add
    Thêm vài ví dụ

---
+++ ClassicCarView/iPadView/Example.h
@@ -0,0 +1,13 @@
+//
+//  Example.h
+//  ClassicCarView
+//
+//  Created by Le Viet Quang on 2/14/14.
+//  Copyright (c) 2014 Le Viet Quang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Example : UIView
+
+@end
---
+++ ClassicCarView/iPadView/Example.m
@@ -0,0 +1,31 @@
+//
+//  Example.m
+//  ClassicCarView
+//
+//  Created by Le Viet Quang on 2/14/14.
+//  Copyright (c) 2014 Le Viet Quang. All rights reserved.
+//
+
+#import "Example.h"
+
+@implementation Example
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
